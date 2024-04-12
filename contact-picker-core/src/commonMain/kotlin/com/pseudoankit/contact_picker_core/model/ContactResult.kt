bdf29b5f2feaba419f73eb408141631082e31856
ankitkumar <lostankit7@gmail.com>
2024-04-12 19:56:40 +0530

    added demo

--- contact-picker-core/src/commonMain/kotlin/com/pseudoankit/contact_picker_core/model/ContactResult.kt
+++ contact-picker-core/src/commonMain/kotlin/com/pseudoankit/contact_picker_core/model/ContactResult.kt
@@ -3,4 +3,10 @@ package com.pseudoankit.contact_picker_core.model
 public sealed interface ContactResult {
     public data class Success(val contacts: List<Contact>) : ContactResult
     public data class Failure(val message: String) : ContactResult
+
+    public val data: List<Contact>?
+        get() = when (this) {
+            is Failure -> null
+            is Success -> contacts
+        }
 }
--- demo/android/src/main/java/com/pseudoankit/contact_picker_demo/MainActivity.kt
+++ demo/android/src/main/java/com/pseudoankit/contact_picker_demo/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.pseudoankit.contact_picker_demo
 
+import android.Manifest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -7,7 +8,13 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.core.app.ActivityCompat
 import com.pseudoankit.contact_picker_core.ContactPickerController
 
 class MainActivity : ComponentActivity() {
@@ -15,17 +22,28 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         ContactPickerController.initialise(this.application)
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.READ_CONTACTS),
+            1
+        );
 
         setContent {
 
+            var contacts by remember {
+                mutableStateOf("")
+            }
+
             LaunchedEffect(Unit) {
-                println("contacts = ${ContactPickerController.fetchAllContacts()}")
+                val items = ContactPickerController.fetchAllContacts()
+                contacts = items.data?.joinToString { it.name }.orEmpty()
             }
 
             Box(
                 modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
             ) {
-                Text(text = "CP")
+                Text(text = contacts)
             }
         }
     }
